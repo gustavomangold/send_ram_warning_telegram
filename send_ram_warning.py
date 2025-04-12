7222439824b171d365d62011b067dbf2fff94692
Gustavo Mangold <gustavocmangold@gmail.com>
2025-04-12 18:35:33 -0300

    add documentation

--- send_ram_warning.py
+++ send_ram_warning.py
@@ -9,17 +9,18 @@ with open("free_output.txt") as file:
     for line in file:
         usage_value = float(line)
 
+# load the chat id and the token from a secure file
 token_id = []
 with open("token_and_id.txt") as file:
     for line in file:
         token_id.append(line)
 
+# ram usage cap value to send a message
 cap_value = .7
-
-print(token_id)
 
 chat_id = token_id[0].strip("\n")
 TOKEN = token_id[1].strip("\n")
+# your message here
 message = 'Atenção! O uso da RAM do pcrites1 está acima de {}%! Atualmente, seu uso é de aproximadamente: {}%'.format(cap_value * 100, 100 * usage_value)
 
 if usage_value > cap_value:
--- sense_ram.sh
+++ sense_ram.sh
@@ -2,8 +2,7 @@ while :
 do
 	free -m | grep Mem: | awk '{print $3/($2)}' >> free_output.txt
 	python3 send_ram_warning.py
-	# wait 5 minutes till next check
-	#
+	# remove temporary file and wait 5 minutes till next check
 	rm free_output.txt
 	sleep 300 
 done
